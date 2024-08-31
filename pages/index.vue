f990b5a1a2c2e98a0563f065bdff378435d94b2e
jcwieme <jean-christophe.wieme@spendesk.com>
2024-08-31 23:52:50 +0200

    def split add tribe + player

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,20 @@
   <div class="def">
     <Box title="Inputs">
       <div class="input">
+        <label for="cibles"> Tribu: </label>
+        <select id="tribu" name="tribu" v-model="selectedAlly">
+          <option value="">Choisir une tribu</option>
+          <option v-for="ally in allies" :key="ally.id" :value="ally.id">{{ ally.tag }}</option>
+        </select>
+      </div>
+      <div class="input" v-if="filteredPlayers.length">
+        <label for="cibles"> Joueur: </label>
+        <select id="joueur" name="joueur" v-model="selectedPlayer">
+          <option value="">Choisir un joueur</option>
+          <option v-for="player in filteredPlayers" :key="player.id" :value="player.id">{{ player.name }}</option>
+        </select>
+      </div>
+      <div class="input">
         <label for="cibles"> Coordonnées des cibles: </label>
         <textarea id="cibles" name="cibles" rows="4" cols="50" v-model="textAreaInput" />
       </div>
@@ -29,6 +43,8 @@
 </template>
 
 <script setup>
+import { onMounted, watch } from 'vue'
+
 const isCopied = ref(false)
 const textAreaInput = ref('')
 const units = ref({
@@ -68,6 +84,13 @@ const results = computed(() => {
   })
 })
 const button = computed(() => (results.value ? (isCopied.value ? 'BBCode copié!' : 'Copier le BBCode') : null))
+const players = ref([])
+const allies = ref([])
+const selectedAlly = ref('')
+const selectedPlayer = ref('')
+const filteredPlayers = computed(() => {
+  return players.value.filter((player) => player.tribe_id === selectedAlly.value)
+})
 
 const copyBBCode = () => {
   const bbCode = results.value
@@ -80,15 +103,78 @@ const copyBBCode = () => {
     .join('\n\n')
 
   navigator.clipboard.writeText(bbCode)
+  // https://fr89.guerretribale.fr/game.php?screen=mail&mode=new&player=1760165&name=Palou1983
+  // open the mail page with the player id and name
+  if (selectedPlayer.value) {
+    const name = players.value.find((player) => player.id === selectedPlayer.value).name
+    window.open(
+      `https://fr89.guerretribale.fr/game.php?screen=mail&mode=new&player=${selectedPlayer.value}&name=${name}`
+    )
+  }
+
   isCopied.value = true
   setTimeout(() => {
     isCopied.value = false
   }, 2000)
 }
+
+// Fetch and convert the ally and player data
+onMounted(async () => {
+  const ally = await fetchAndConvertToJSON('https://fr89.guerretribale.fr/map/ally.txt', 'ally')
+  const player = await fetchAndConvertToJSON('https://fr89.guerretribale.fr/map/player.txt', 'player')
+  allies.value = ally.sort(function (a, b) {
+    return a.tag.localeCompare(b.tag)
+  })
+  players.value = player
+})
+async function fetchAndConvertToJSON(url, type) {
+  try {
+    // Fetch the text file from the URL
+    const response = await fetch(url)
+    const textData = await response.text()
+
+    // Split the text into lines
+    const lines = textData.trim().split('\n')
+
+    // Convert lines to JSON
+    const jsonArray = type === 'ally' ? mapAlly(lines) : mapPlayer(lines)
+
+    // Log the JSON result to the console
+    return jsonArray
+  } catch (error) {
+    console.error('Error fetching or processing the file:', error)
+  }
+}
+
+const mapAlly = (lines) => {
+  return lines.map((line) => {
+    const [id, name, tag] = line.split(',')
+
+    return {
+      id: parseInt(id),
+      name: decodeURIComponent(name.replace(/\+/g, ' ')),
+      tag: decodeURIComponent(tag.replace(/\+/g, ' ')),
+    }
+  })
+}
+
+const mapPlayer = (lines) => {
+  return lines.map((line) => {
+    const [id, name, tribe_id] = line.split(',')
+
+    return {
+      id: parseInt(id),
+      name: decodeURIComponent(name.replace(/\+/g, ' ')),
+      tribe_id: parseInt(tribe_id),
+    }
+  })
+}
+
 // player ID + name
 // How to get player id ?
 // https://fr89.guerretribale.fr/game.php?screen=mail&mode=new&player=1760165&name=Palou1983
 // https://fr89.guerretribale.fr/map/player.txt
+// https://fr89.guerretribale.fr/map/ally.txt
 </script>
 
 <style>
